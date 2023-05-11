a874b772f8a1c73621809891230c9667bd7b697b
lyokoth <97857899+lyokoth@users.noreply.github.com>
2023-05-11 14:11:48 -0400

    update ui

--- src/App.js
+++ src/App.js
@@ -1,96 +1,33 @@
-import {useEffect, useState} from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-node";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+
+const spotify = new SpotifyWebApi();
+
 
 function App() {
-  const CLIENT_ID = "513546ee9bef4e9caae7a75eb3c796a4"
-  const REDIRECT_URI = "http://localhost:3000"
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  const RESPONSE_TYPE = "token"
+  const [token, setToken] = useState();
 
-  const [token, setToken] = useState("")
-  const [searchKey, setSearchKey] = useState("")
-  const [artists, setArtists] = useState([])
-  
   useEffect(() => {
-    const hash = window.location.hash
-          let token = window.localStorage.getItem("token")
-  
-          if (!token && hash) {
-              token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-  
-              window.location.hash = ""
-              window.localStorage.setItem("token", token)
-          }
-  
-          setToken(token)
-  
-      }, [])
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
 
-      const logout = () => {
-        setToken("")
-        window.localStorage.removeItem("token")
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
     }
 
-    const searchArtists = async (e) => {
-      e.preventDefault()
-      const {data} = await axios.get("https://api.spotify.com/v1/search", {
-          headers: {
-              Authorization: `Bearer ${token}`
-          },
-          params: {
-              q: searchKey,
-              type: "artist"
-          }
-      })
+    console.log("token", token);
+  }, []);
 
-      setArtists(data.artists.items)
-  }
-
-  
-
-
-  const renderArtists = () => {
-    return artists.map(artist =>
-      <div key={artist.id}>
-        {artist.images.length ? <img width={"75%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
-        {artist.name}
-
-      </div>
-    )
-  }
-  
-
-    return (
-        <div className="App">
-            <header className="App-header">
-              <h1>Welcome to</h1>
-              <h2>⭐Reactify⭐</h2>
-              {!token ?
-                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}><button type={"submit"} class="submit-button">Login</button>
-                       </a>
-                    : <button class="logout" onClick={logout}>Logout</button>}
-
-              
-              {token ?
-                    <form onSubmit={searchArtists}>
-                        <input type="text" class="search" onChange={e => setSearchKey(e.target.value)}/>
-                        <button type={"submit"} class="submit-button">Search</button>
-                    </form>
-
-                    : <button type={"submit"} class="submit-button">Login to Spotify</button>
-                }
-
-              {renderArtists()}
-
-
-            </header>
-        </div>
-    );
+  return  <div className="app">{token ? <h1>Logged In@</h1>: <Login/>}</div>;
 }
 
-
-
 export default App;
-
 
